c57b0e4d654392b501a38efb3998aa9ba37abba5
Teresa Johnson <tejohnson@google.com>
2020-09-29 15:31:11 -0700

    [MemProf] Allow the binary to specify the profile output filename
    This will allow the output directory to be specified by a build time
    option, similar to the directory specified for regular PGO profiles via
    -fprofile-generate=. The memory profiling instrumentation pass will
    set up the variable. This is the same mechanism used by the PGO
    instrumentation and runtime.
    
    Depends on D87120 and D89629.
    
    Differential Revision: https://reviews.llvm.org/D89086

--- compiler-rt/test/memprof/TestCases/log_path_test.cpp
+++ compiler-rt/test/memprof/TestCases/log_path_test.cpp
@@ -31,6 +31,24 @@
 char __memprof_profile_filename[] = xstr(PROFILE_NAME_VAR);
 #endif
 
+// Specifying the log name via the __memprof_profile_filename variable.
+// RUN: %clangxx_memprof  %s -o %t -DPROFILE_NAME_VAR="%t.log2"
+// RUN: rm -f %t.log2.*
+// RUN: %run %t 2> %t.out
+// RUN: FileCheck %s --check-prefix=CHECK-GOOD < %t.log2.*
+
+// Check that the log_path option overrides the log name set via the
+// __memprof_profile_filename variable.
+// RUN: rm -f %t.log.*
+// RUN: %env_memprof_opts=log_path=%t.log %run %t 2> %t.out
+// RUN: FileCheck %s --check-prefix=CHECK-GOOD < %t.log.*
+
+#ifdef PROFILE_NAME_VAR
+#define xstr(s) str(s)
+#define str(s) #s
+char __memprof_profile_filename[] = xstr(PROFILE_NAME_VAR);
+#endif
+
 #include <stdlib.h>
 #include <string.h>
 int main(int argc, char **argv) {
